522c08237e151e5136bd778fb8568b61b124ba27
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-06-25 00:16:40 +0600

    added prompt delete api

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ const run = async () => {
       res.send(result);
     });
 
+    app.delete("/api/prompt/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await promptCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
